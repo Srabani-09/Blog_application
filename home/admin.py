df18af81076272693dc18196fa9033c18d6f920d
Srabani-09 <srabanibehera1234@gmail.com>
2025-02-22 11:41:07 +0530

    first commit

---
+++ home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import BlogModel
+# Register your models here.
+# from .models import BlogModel, Profile
+
+
+admin.site.register(BlogModel)
+# admin.site.register(Profile)
